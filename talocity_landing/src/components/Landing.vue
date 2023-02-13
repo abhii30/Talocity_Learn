aaf47d687792ac83b79662cc19e64fd1cd8e92d7
Abhii30 <akumar300599@gmail.com>
2023-02-13 15:45:14 +0530

    dark theme

--- talocity_landing/src/components/Landing.vue
+++ talocity_landing/src/components/Landing.vue
@@ -2,7 +2,69 @@
 
 <template>
   <div class="landing">
-    <div class="landingLeft">hello</div>
+    <div class="landingLeft">
+      <div class="col3">
+        <div class="box1"><img src="./icons/1.png" alt="" /></div>
+        <div class="box1">
+          <div class="boxColor" style="background-color: #e7fafe"></div>
+        </div>
+        <div class="box1"><img src="./icons/2.png" alt="" /></div>
+      </div>
+      <div class="col3">
+        <div class="box1">
+          <div class="boxColor" style="background-color: #f3f2ff"></div>
+        </div>
+        <div class="box1"><img src="./icons/3.png" alt="" /></div>
+        <div class="box1">
+          <div class="col2">
+            <div class="row2">
+              <div class="boxColor" style="background-color: #e2e3fe"></div>
+            </div>
+            <div class="row2"></div>
+          </div>
+          <div class="col2">
+            <div class="row2"></div>
+            <div class="row2"><img src="./icons/4.png" alt="" /></div>
+          </div>
+        </div>
+      </div>
+      <div class="col3">
+        <div class="box1">
+          <div class="col2">
+            <div class="row2"></div>
+            <div class="row2"><img src="./icons/5.png" alt="" /></div>
+          </div>
+          <div class="col2">
+            <div class="row2"></div>
+            <div class="row2"></div>
+          </div>
+        </div>
+        <div class="box1">
+          <div class="col2">
+            <div class="row2"></div>
+            <div class="row2">
+              <div class="boxColor" style="background-color: #dffcfe"></div>
+            </div>
+          </div>
+          <div class="col2">
+            <div class="row2">
+              <div class="boxColor" style="background-color: #e2e3fe"></div>
+            </div>
+            <div class="row2"></div>
+          </div>
+        </div>
+        <div class="box1">
+          <div class="col2">
+            <div class="row2"><img src="./icons/6.png" alt="" /></div>
+            <div class="row2"></div>
+          </div>
+          <div class="col2">
+            <div class="row2"></div>
+            <div class="row2"></div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="landingRight">
       <div class="loginContainer">
         <form action="" class="loginForm">
@@ -24,7 +86,7 @@
             <input type="password" id="password" />
           </div>
           <button type="submit">Login</button>
-          <p>Don't have an account?  <a href="#">Contact Sales</a></p>
+          <p>Don't have an account? <a href="#">Contact Sales</a></p>
         </form>
       </div>
     </div>
